e0794283b11fa2ba24f6fce58b309bce04775d62
JordanS2209 <jssalcedoc49@gmail.com>
2025-12-01 16:42:44 -0500

    Agregar archivos de proyecto.

---
+++ Zoologico.Api/Migrations/20251201211452_v01zoo.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Zoologico.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v01zoo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Especies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Especies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Razas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Razas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    año = table.Column<int>(type: "integer", nullable: false),
+                    color = table.Column<string>(type: "text", nullable: false),
+                    EspecieId = table.Column<int>(type: "integer", nullable: false),
+                    RazaId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animales", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animales_Especies_EspecieId",
+                        column: x => x.EspecieId,
+                        principalTable: "Especies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Animales_Razas_RazaId",
+                        column: x => x.RazaId,
+                        principalTable: "Razas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animales_EspecieId",
+                table: "Animales",
+                column: "EspecieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animales_RazaId",
+                table: "Animales",
+                column: "RazaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animales");
+
+            migrationBuilder.DropTable(
+                name: "Especies");
+
+            migrationBuilder.DropTable(
+                name: "Razas");
+        }
+    }
+}
---
+++ Zoologico.modelo/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoologico.modelo
+{
+    public class Animal
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int año { get; set; }
+        public string color { get; set; }
+
+        // claves foraneas 
+        public int EspecieId { get; set; }
+        public int RazaId { get; set; }
+
+        // propiedades de navegacion
+        public Especie? Especie { get; set; }
+        public Raza? Raza { get; set; }
+
+    }
+}
