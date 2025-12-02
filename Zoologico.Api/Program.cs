6d161efacf54a978798f5eb0525ab878ca94df35
JordanS2209 <jssalcedoc49@gmail.com>
2025-12-01 19:23:14 -0500

    cambios para conexion base de datos 2

--- Zoologico.Api/Program.cs
+++ Zoologico.Api/Program.cs
@@ -1,10 +1,28 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+
+var SqlServerConeccion = builder.Configuration.GetConnectionString("PostgresDbContext");
+var PostgresConeccion = builder.Configuration.GetConnectionString("SqlServerDbContext");
+// Coneccion a PostgreSQL ----------------------------------------------------------------------------
 builder.Services.AddDbContext<PostgresDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresDbContext") ?? throw new InvalidOperationException("Connection string 'PostgresDbContext' not found.")));
+    options.UseNpgsql( PostgresConeccion, sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly(typeof(PostgresDbContext).Assembly.FullName);
+        sqlOptions.MigrationsHistoryTable("_EFMigrationsHistory_PostgresSQL");
+    
+    }));
+// Coneccion a SQL Server ----------------------------------------------------------------------------
 builder.Services.AddDbContext<SqlServerDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerDbContext") ?? throw new InvalidOperationException("Connection string 'SqlServerDbContext' not found.")));
+    options.UseSqlServer(SqlServerConeccion, sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly(typeof(SqlServerDbContext).Assembly.FullName);
+        sqlOptions.MigrationsHistoryTable("_EFMigrationsHistory_SqlServer");
+
+    }));
+
+//builder.Services.AddDbContext<SqlServerDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerDbContext") ?? throw new InvalidOperationException("Connection string 'SqlServerDbContext' not found.")));
 //builder.Services.AddDbContext<ZoologicoApiContext>(options =>
 //    options.UseNpgsql(builder.Configuration.GetConnectionString("ZoologicoApiContext") ?? throw new InvalidOperationException("Connection string 'ZoologicoApiContext' not found.")));
 
